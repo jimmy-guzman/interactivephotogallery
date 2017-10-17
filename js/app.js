cb56d015d7355d234fbe4230b637eb017e81df62
Jimmy Guzman <jimmy.guzman@outlook.com>
2017-10-16 23:15:02 -0500

    added images, set layout

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#lightgallery").lightGallery( {
+      getCaptionFromTitleOrAlt: true,
+      download: false,
+      counter: false,
+      toogleThumb: false,
+      showThumbByDefault: false,
+      fullScreen: false,
+      width: '60%'
+    });
+});
