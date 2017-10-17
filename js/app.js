f3e8a51bad3034f368bf29d0c903bc90a1b354a6
Jimmy Guzman <jimmy.guzman@outlook.com>
2017-10-17 18:05:53 -0500

    added search function and titles

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//lightGallery function
 $(document).ready(function() {
     $("#lightgallery").lightGallery( {
       getCaptionFromTitleOrAlt: true,
@@ -9,3 +10,23 @@ $(document).ready(function() {
       width: '60%'
     });
 });
+
+
+//search function
+function searchBox() {
+    // Variable declaration
+    var input, filter, a, img;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    a = document.getElementsByClassName('picture');
+
+    // Loop through all a(picture) items, and hide a(pictures) who don't match the search criteria
+    for (var i = 0; i < a.length; i++) {
+        img = a[i].getElementsByTagName("img")[0];
+        if (img.alt.toUpperCase().indexOf(filter) > -1 || img.title.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
